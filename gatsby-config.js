57032ae08a1cf569a3814b70d68845af321f813a
John Naylor <jonaylor89@gmail.com>
2020-05-04 11:28:18 -0400

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -183,5 +183,11 @@ module.exports = {
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-force-trailing-slashes`,
         `gatsby-plugin-offline`,
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: `UA-165434800-1`,
+            },
+        },
     ],
 }
